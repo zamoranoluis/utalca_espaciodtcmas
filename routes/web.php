016df1c23c31834c9a0c0c89685f24c1fc49d5b1
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2024-11-16 15:02:00 -0300

    feat: redirreccion de autenticacion si ya pasó sus correspondientes etapas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\RedirigirSiPasoIniciarSesionMiddleware;
 use App\Http\Middleware\Verificar2FA;
 use App\Http\Middleware\VerificarAutenticado;
 use App\Livewire\Autenticacion\IniciarSesion;
@@ -9,12 +10,25 @@ use App\Livewire\Publica\Inicio;
 
 Route::get('/', Inicio::class)->name('inicio');
 
-Route::get('/autenticacion/iniciar-sesion',
-    IniciarSesion::class)
-    ->name('iniciar-sesion');
+// se pone este middleware, sólo para esta ruta con la finalidad de
+// redirigir al siguiente paso si ya se autenticó
+Route::middleware([
+    RedirigirSiPasoIniciarSesionMiddleware::class,
+])->group(function () {
+
+    Route::get('/autenticacion/iniciar-sesion',
+        IniciarSesion::class)
+        ->name('iniciar-sesion');
+
+});
 
 Route::middleware([
     VerificarAutenticado::class,
+    // lo mismo que el middleware único anterior, verifica si ya pasó el
+    // 2fa para redirigir a Dashboard. Es importante destacar que
+    // si se quiere redirigir 'por rol' a diferentes dashboard, debe
+    // cambiarse esta lógica
+    \App\Http\Middleware\RedirigirSiPaso2FA::class,
 ])->group(function () {
 
     Route::get('/autenticacion/verificacion-de-identidad',
