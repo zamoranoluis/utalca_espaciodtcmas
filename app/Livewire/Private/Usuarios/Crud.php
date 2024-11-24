ef78cb1d9676cf4e937843ee4a4de3ab46d3ce12
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2024-11-24 19:15:00 -0300

    dstyle: diseño esperado para pagina 'usuario', responsivo

--- app/Livewire/Private/Usuarios/Crud.php
+++ app/Livewire/Private/Usuarios/Crud.php
@@ -2,12 +2,65 @@
 
 namespace App\Livewire\Private\Usuarios;
 
+use App\Models\User;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Crud extends Component
 {
+    public $usuarios;
+
+    public $cantidadUsuarios;
+
+    public $cantidadPaginas;
+
+    public $paginaActual = 0;
+
+    public function cambiarPagina(int $numeroPagina)
+    {
+        $this->paginaActual = $numeroPagina;
+        $this->obtenerUsuarios();
+    }
+
+    public function avanzarPagina()
+    {
+        if ($this->paginaActual + 1 < $this->cantidadPaginas) {
+            $this->cambiarPagina($this->paginaActual + 1);
+        } else {
+            toastr()->error('Llegaste al limite máximo de páginas');
+        }
+    }
+
+    public function retrocederPagina()
+    {
+        if ($this->paginaActual - 1 >= 0) {
+            $this->cambiarPagina($this->paginaActual - 1);
+        } else {
+            toastr()->error('Llegaste al limite mínimo de páginas');
+        }
+    }
+
+    public function obtenerUsuarios()
+    {
+        $filasPorPagina = config('tablas.filas_por_pagina');
+
+        $this->usuarios = \DB::table('users')
+            ->select('nombres', 'apellidos', 'email', 'habilitado')
+            ->orderBy('email', 'asc')
+            ->skip($filasPorPagina * ($this->paginaActual))
+            ->take($filasPorPagina)
+            ->get();
+    }
+
+    public function mount()
+    {
+        $this->cantidadUsuarios = User::count();
+        $filasPorPagina = config('tablas.filas_por_pagina');
+        $this->cantidadPaginas = ceil($this->cantidadUsuarios / $filasPorPagina);
+        $this->obtenerUsuarios();
+    }
+
     #[Title('Usuarios')]
     #[Layout('components.layouts.dashboard')]
     public function render()
--- resources/views/livewire/private/usuarios/crud.blade.php
+++ resources/views/livewire/private/usuarios/crud.blade.php
@@ -1,22 +1,54 @@
 <div class="dashboard-layout__crud__grid">
     <!--barra busqueda-->
     <div class="dashboard-layout__crud__grid__busqueda">
-        <label>Busqueda:</label>
-        <input>
+        <div class="dashboard-layout__crud__grid__busqueda__flex">
+            <label>Busqueda:</label>
+            <input>
+        </div>
     </div>
 
     <!--acciones-->
     <div class="dashboard-layout__crud__grid__acciones">
-        <button>Agregar usuario</button>
+        <button>Agregar</button>
     </div>
 
     <!-- tabla -->
     <div class="dashboard-layout__crud__grid__tabla">
+        <table>
+            <tr>
+                <th>Email</th>
+                <th>Nombres</th>
+                <th>Apellido</th>
+                <th>Estado habilitado</th>
+                <th class="acciones">Acciones</th>
+            </tr>
+
+            @foreach($usuarios as $usuario)
+                <tr>
+                    <td>{{$usuario->email}}</td>
+                    <td>{{$usuario->nombres}}</td>
+                    <td>{{$usuario->apellidos}}</td>
+                    <td>{{$usuario->habilitado ? "Habilitado" : "Deshabilitado"}}</td>
+                    <td>
+                        <button>Editar</button>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
 
     </div>
 
     <!-- paginador -->
     <div class="dashboard-layout__crud__grid__paginador">
-
+        <div class="dashboard-layout__crud__grid__paginador__flex">
+            <button {{ $this->paginaActual == 0 ? 'disabled' : '' }} wire:click="retrocederPagina"><</button>
+            @for($i=0; $i<$cantidadPaginas; $i++)
+                <button wire:click="cambiarPagina({{$i}})">
+                    {{$i+1}}
+                </button>
+            @endfor
+            <button {{ $this->paginaActual == $cantidadPaginas - 1 ? 'disabled' : '' }} wire:click="avanzarPagina">>
+            </button>
+        </div>
     </div>
 </div>
